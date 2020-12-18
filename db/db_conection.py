8ff86789b09983f0129036800ccfa7f3f63d85ab
Roberto Londoño <roberthdrums@gmail.com>
2020-12-17 19:36:58 -0500

    cambios clase 21

--- db/db_conection.py
+++ db/db_conection.py
@@ -4,6 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #Creando Motor y Conexion con la Base de Datos
+# TODO: Con la ayuda de pgAdmin debes crear una base de datos
+# y la BD debes crearle un esquema
+# TODO: Debes cambiar con tu usuario y contraseña de PostgreSQL
+# además del host, puerto y nombre de la base de datos que hayas definido
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/misiontic"
 engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -11,6 +15,8 @@ engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, 
                             autoflush=False, 
                             bind=engine)
+
+# En get_db inyectamos la dependencia SessionLocal
 def get_db():
     db = SessionLocal()
     try:
@@ -21,4 +27,7 @@ def get_db():
 
 #Creando Base para la creacion de los modelos
 Base = declarative_base()
+
+# TODO: Reemplazar el nombre del esquema creado 
+# en la base de datos
 Base.metadata.schema = "cajerodb"
--- main.py
+++ main.py
@@ -7,5 +7,17 @@ from routers.transaction_router import router as router_transactions
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+# Incluimos nuestros dos endpoints en api 
 app.include_router(router_users)
 app.include_router(router_transactions)
